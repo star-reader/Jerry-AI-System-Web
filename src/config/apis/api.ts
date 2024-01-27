9c875db2ccf1eb8b7b8d9b976030e92ef99cf821
star-reader <75555433+star-reader@users.noreply.github.com>
2024-01-27 21:19:18 +0800

    Update api.ts

--- src/config/apis/api.ts
+++ src/config/apis/api.ts
@@ -1,8 +1,8 @@
 export default {
-    getAllList: `https://api.starjin.top/ai/list`,
-    getDetailDialog: `https://api.starjin.top/ai/dialog`,
-    deleteDialog: `https://api.starjin.top/ai/delete`,
-    stopChat: `https://api.starjin.top/ai/stop`,
-    wsServer: `wss://api.starjin.top/ai/gpt`,
-    login: `https://api.starjin.top/ai/login-token`
+    getAllList: ``,
+    getDetailDialog: ``,
+    deleteDialog: ``,
+    stopChat: ``,
+    wsServer: ``,
+    login: ``
 }
